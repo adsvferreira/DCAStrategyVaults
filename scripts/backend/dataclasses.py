27ca1c2b96374fc934ebfba009998bc5eba451ef
adsvferreira <adsv.ferreira400@gmail.com>
2023-08-30 13:38:53 +0100

    add StrategyFetcher

--- scripts/backend/dataclasses.py
+++ scripts/backend/dataclasses.py
@@ -1,17 +1,18 @@
+from typing import List
 from dataclasses import dataclass
 
 @dataclass
 class StrategyVault:
     address:str
-    id_active: bool
+    is_active: bool
     creator: str
     deposit_token_address: str
-    token_addresses_to_buy: list[str]
-    tokens_to_buy_decimals: list[int]
-    buy_amounts: list[int]
-    depositor_addresses: list[str]
+    token_addresses_to_buy: List[str]
+    tokens_to_buy_decimals: List[int]
+    buy_amounts: List[int]
+    depositor_addresses: List[str]
     buy_frequency_timestamp: int
-    last_update_timestamp = int
+    last_update_timestamp: int
 
     
 
--- scripts/backend/main.py
+++ scripts/backend/main.py
@@ -1,3 +1,12 @@
+from scripts.backend.strategy_fetcher import StrategyFetcher
+
+# EXECUTE IN PROJECT ROOT:
+# brownie run scripts/backend/main.py --network arbitrum-main-fork --interactive
+
 def main():
-    while True:
-        pass
+    strategy_fetcher = StrategyFetcher()
+    all_vault_addresses=strategy_fetcher.fetch_vault_addresses()
+    print("ALL VAULT ADDRESSES:", all_vault_addresses)
+    print()
+    all_vaults = strategy_fetcher.fetch_vaults(all_vault_addresses)
+    print("ALL VAULTS:", all_vaults)
