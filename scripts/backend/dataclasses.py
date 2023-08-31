7bad0fc6ffa1600b0a26c7e49fb299c1fa61edc8
RodrigoRalhaMoreira <rr.moreira@campus.fct.unl.pt>
2023-08-31 14:04:56 +0100

    added scheduler need private functions

--- scripts/backend/dataclasses.py
+++ scripts/backend/dataclasses.py
@@ -1,9 +1,10 @@
 from typing import List
 from dataclasses import dataclass
 
+
 @dataclass
 class StrategyVault:
-    address:str
+    address: str
     creator: str
     deposit_token_address: str
     token_addresses_to_buy: List[str]
@@ -12,6 +13,3 @@ class StrategyVault:
     depositor_addresses: List[str]
     buy_frequency_timestamp: int
     last_update_timestamp: int
-
-    
-
--- scripts/backend/main.py
+++ scripts/backend/main.py
@@ -1,22 +1,24 @@
-from time import time
+import time
 from scripts.backend.strategy_fetcher import StrategyFetcher
 from scripts.backend.controller_executor import ControllerExecutor
-from scripts.backend.helpers import buy_frequency_enum_to_seconds_map
 
 # EXECUTE IN PROJECT ROOT:
 # brownie run scripts/backend/main.py --network arbitrum-main-fork --interactive
 
+
 def main():
-    bot_start_time = time()
     strategy_fetcher = StrategyFetcher()
     controller_executor = ControllerExecutor()
-    all_vault_addresses=strategy_fetcher.fetch_vault_addresses()
+    all_vault_addresses = strategy_fetcher.fetch_vault_addresses()
     print("ALL VAULT ADDRESSES:", all_vault_addresses)
     all_vaults = strategy_fetcher.fetch_vaults(all_vault_addresses)
-    # print("ALL VAULTS:", all_vaults)
-
+    
+    # filtered vaults because of reverted transactions
+    all_vaults = [vault for vault in all_vaults if vault.address != "0xCaDf5ba5AB9Cb2f6206d98D7Ae45157D79B66A0A"]
+    print("VAULTS RUNNING")
+    print(all_vaults)
+    
     print("UPDATING STRATEGY VAULTS...")
-
     for vault in all_vaults:
         for depositor_address in vault.depositor_addresses:
             try:
@@ -29,3 +31,29 @@ def main():
             print()
             tx.wait(1)
     print("STRATEGY VAULTS UPDATED!!!!!")
+    time.sleep(300)
+    print("STARTING WHILE TRUE....")
+    
+    while True:
+        print("STARTING ITERATION...")
+        current_time = time.time()
+        print(f"Current Time: {current_time}")
+
+        print("UPDATING STRATEGY VAULTS...")
+        for vault in all_vaults:
+            # Check if the difference between current time and last update time is greater than or equal to the interval
+            print(f"Vault last updated timestamp: {vault.last_update_timestamp}")
+            if current_time - vault.last_update_timestamp >= vault.buy_frequency_timestamp:
+                for depositor_address in vault.depositor_addresses:
+                    try:
+                        tx = controller_executor.trigger_strategy_action(vault.address, depositor_address)
+                        print(f"WALLET: {depositor_address} BALANCES SWAPPED AND SENT TO DESTINATION WALLET")
+                    except Exception:
+                        print(f"TRANSACTION FAILED FOR WALLET: {depositor_address}")
+                    print("VAULT DETAILS:")
+                    print(vault)
+                    print()
+                    tx.wait(1)
+        print("STRATEGY VAULTS UPDATED OF WHILE TRUE!!!!!")
+        time.sleep(300)
+        print("ENDING SLEEP TIME...")
